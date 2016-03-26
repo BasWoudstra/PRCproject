acf446debfe389f49bf70f633531603a9d2506d4
BasWoudstra <bashummer@gmail.com>
2016-03-26 15:48:17 +0100

    testcase cat en dog 2keer uitgevoerd en adimistratie test toegevoegd
    nog niet gerund, kreeg foutmelding bij "public override void
    CalcPrice();" in animal. kan geen public overwrite zijn zegt i. moet
    abstract zijn

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -9,7 +9,8 @@ namespace UnitTestProject1
     {
         Cat Testcat;
         Dog Testdog;
-
+        Administration TestAdministration = new Administration();
+        
         private void MakeCat()
         {
         //make a cat
@@ -22,24 +23,49 @@ namespace UnitTestProject1
         //act
         Testcat = new Cat(catchipNumber, catDateOfBirth, catname, catbadHabits);
         }
-        
+        private void MakeAnotherCat()
+        {
+            //make a cat
+            //range
+            string catname = "newCat";
+            int catchipNumber = 85725;
+            SimpleDate catDateOfBirth = new SimpleDate(7, 5, 2000);
+            string catbadHabits = "";
+
+            //act
+            Testcat = new Cat(catchipNumber, catDateOfBirth, catname, catbadHabits);
+        }
+
         private void MakeDog()
         {
-        //make a dog
-        //range
-        string dogname = "thisdog";
-        int dogchipNumber = 123;
-        SimpleDate dogDateOfBirth = new SimpleDate(2, 5, 2015);
-        SimpleDate dogDateOfWalk = new SimpleDate(2, 6, 2015);
+            //make a dog
+            //range
+            string dogname = "thisdog";
+            int dogchipNumber = 123;
+            SimpleDate dogDateOfBirth = new SimpleDate(2, 5, 2015);
+            SimpleDate dogDateOfWalk = new SimpleDate(2, 6, 2015);
 
-        //act
-        Testdog = new Dog(dogchipNumber, dogDateOfBirth, dogname, dogDateOfWalk);
+            //act
+            Testdog = new Dog(dogchipNumber, dogDateOfBirth, dogname, dogDateOfWalk);
 
         }
-        
-        //cat
+        private void MakeAnotherDog()
+        {
+            //make a dog
+            //range
+            string dogname = "newdog";
+            int dogchipNumber = 54685;
+            SimpleDate dogDateOfBirth = new SimpleDate(7,7,1990);
+            SimpleDate dogDateOfWalk = new SimpleDate(8,5,2005);
+
+            //act
+            Testdog = new Dog(dogchipNumber, dogDateOfBirth, dogname, dogDateOfWalk);
+
+        }
+
+        //test class: cat
         [TestMethod]
-        public void Test_CatNumber()
+        public void Test_CatNumber1()
         {
             MakeCat();
             //assert
@@ -48,7 +74,16 @@ namespace UnitTestProject1
             Assert.AreEqual(expectedNumber, accualNumber);
         }
         [TestMethod]
-        public void Test_CatDate()
+        public void Test_CatNumber2()
+        {
+            MakeAnotherCat();
+            //assert
+            int expectedNumber = 85725;
+            int accualNumber = Testcat.ChipRegistrationNumber;
+            Assert.AreEqual(expectedNumber, accualNumber);
+        }
+        [TestMethod]
+        public void Test_CatDate1()
         {
             MakeCat();
             //assert
@@ -57,7 +92,16 @@ namespace UnitTestProject1
             Assert.AreEqual(wantedDate, thisDate);
         }
         [TestMethod]
-        public void test_CatName()
+        public void Test_CatDate2()
+        {
+            MakeAnotherCat();
+            //assert
+            string thisDate = Convert.ToString(Testcat.DateOfBirth);
+            string wantedDate = Convert.ToString(new SimpleDate(7, 5, 2000));
+            Assert.AreEqual(wantedDate, thisDate);
+        }
+        [TestMethod]
+        public void test_CatName1()
         {
             MakeCat();
             //assert
@@ -66,11 +110,29 @@ namespace UnitTestProject1
             Assert.AreEqual(expectedname, accualname);
         }
         [TestMethod]
-        public void test_CatHabits()
+        public void test_CatName2()
+        {
+            MakeAnotherCat();
+            //assert
+            string expectedname = "newCat";
+            string accualname = Testcat.Name;
+            Assert.AreEqual(expectedname, accualname);
+        }
+        [TestMethod]
+        public void test_CatHabits1()
         {
             MakeCat();
             //assert
             string expectedHabit = "kan de kattebak niet vinden";
+            string accualHabit = Testcat.BadHabits;
+            Assert.AreEqual(expectedHabit, accualHabit);
+        }
+        [TestMethod]
+        public void test_CatHabits2()
+        {
+            MakeAnotherCat();
+            //assert
+            string expectedHabit = "";
             string accualHabit = Testcat.BadHabits;
             Assert.AreEqual(expectedHabit, accualHabit);
         }
@@ -84,9 +146,29 @@ namespace UnitTestProject1
             string accualnoise = Testcat.makeNoise();
             Assert.AreEqual(expectednoise, accualnoise);
         }
-        //dog
         [TestMethod]
-        public void Test_DogNumber()
+        public void Test_CatGetPrice1()
+        {
+            MakeCat();
+
+            //assert
+            decimal expectedPrice = 33;
+            decimal accualPrice = Testcat.Price;
+            Assert.AreEqual(expectedPrice, accualPrice);
+        }
+        [TestMethod]
+        public void Test_CatGetPrice2()
+        {
+            MakeAnotherCat();
+
+            //assert
+            decimal expectedPrice = 60;
+            decimal accualPrice = Testcat.Price;
+            Assert.AreEqual(expectedPrice, accualPrice);
+        }
+        //test class: dog
+        [TestMethod]
+        public void Test_DogNumber1()
         {
             MakeDog();
             //assert
@@ -95,16 +177,34 @@ namespace UnitTestProject1
             Assert.AreEqual(expectedresult, accualresult);
         }
         [TestMethod]
-        public void Test_DogDateBirth()
+        public void Test_DogNumber2()
+        {
+            MakeAnotherDog();
+            //assert
+            int expectedresult = 54685;
+            int accualresult = Testdog.ChipRegistrationNumber;
+            Assert.AreEqual(expectedresult, accualresult);
+        }
+        [TestMethod]
+        public void Test_DogDateBirth1()
         {
             MakeDog();
             //assert
-            string expectedDate =Convert.ToString( new SimpleDate(2, 5, 2015));
-            string accualDate = Convert.ToString( Testdog.DateOfBirth);
+            string expectedDate = Convert.ToString(new SimpleDate(2, 5, 2015));
+            string accualDate = Convert.ToString(Testdog.DateOfBirth);
             Assert.AreEqual(expectedDate, accualDate);
         }
         [TestMethod]
-        public void Test_DogName()
+        public void Test_DogDateBirth2()
+        {
+            MakeAnotherDog();
+            //assert
+            string expectedDate = Convert.ToString(new SimpleDate(7, 7, 1990));
+            string accualDate = Convert.ToString(Testdog.DateOfBirth);
+            Assert.AreEqual(expectedDate, accualDate);
+        }
+        [TestMethod]
+        public void Test_DogName1()
         {
             MakeDog();
             //assert
@@ -113,12 +213,30 @@ namespace UnitTestProject1
             Assert.AreEqual(expectedName, accualName);
         }
         [TestMethod]
-        public void Test_DogWalk()
+        public void Test_DogName2()
+        {
+            MakeAnotherDog();
+            //assert
+            string expectedName = "newdog";
+            string accualName = Testdog.Name;
+            Assert.AreEqual(expectedName, accualName);
+        }
+        [TestMethod]
+        public void Test_DogWalk1()
         {
             MakeDog();
             //assert
-            string expectedDate =Convert.ToString( new SimpleDate(2, 6, 2015));
-            string accualDate =Convert.ToString( Testdog.LastWalkDate );
+            string expectedDate = Convert.ToString(new SimpleDate(2, 6, 2015));
+            string accualDate = Convert.ToString(Testdog.LastWalkDate);
+            Assert.AreEqual(expectedDate, accualDate);
+        }
+        [TestMethod]
+        public void Test_DogWalk2()
+        {
+            MakeAnotherDog();
+            //assert
+            string expectedDate = Convert.ToString(new SimpleDate(8, 5, 2005));
+            string accualDate = Convert.ToString(Testdog.LastWalkDate);
             Assert.AreEqual(expectedDate, accualDate);
         }
         [TestMethod]
@@ -129,6 +247,119 @@ namespace UnitTestProject1
             string expectednoise = "Woof";
             string accualnoise = Testdog.makeNoise();
             Assert.AreEqual(expectednoise, accualnoise);
+        }
+        [TestMethod]
+        public void Test_DogPrice1()
+        {
+            MakeDog();
+
+            //assert
+            decimal expectedPrice = 200;
+            decimal accualPrice = Testdog.Price;
+            Assert.AreEqual(expectedPrice, accualPrice);
+        }
+        [TestMethod]
+        public void Test_DogPrice2()
+        {
+            MakeAnotherDog();
+
+            //assert
+            decimal expectedPrice = 350;
+            decimal accualPrice = Testdog.Price;
+            Assert.AreEqual(expectedPrice, accualPrice);
+        }
+        //test class: administration
+        [TestMethod]
+        public void Test_AdministrationAddCat()
+        {
+            //range
+            string catname = "thiscat";
+            int catchipNumber = 123;
+            SimpleDate catDateOfBirth = new SimpleDate(2, 5, 2015);
+            string catbadHabits = "kan de kattebak niet vinden";
+            //act
+            Animal newtestcat = new Cat(catchipNumber, catDateOfBirth, catname, catbadHabits);
+            //assert
+            bool expectedresult = true;
+            bool accualresult = TestAdministration.Add(newtestcat);
+            Assert.AreEqual(expectedresult, accualresult);
+        }
+        [TestMethod]
+        public void Test_AdministrationAddDog()
+        {
+            //range
+            string dogname = "thisdog";
+            int dogchipNumber = 123;
+            SimpleDate dogDateOfBirth = new SimpleDate(2, 5, 2015);
+            SimpleDate dogDateOfWalk = new SimpleDate(2, 6, 2015);
+            //act
+            Animal newtestdog = new Dog(dogchipNumber, dogDateOfBirth, dogname, dogDateOfWalk);
+            //assert
+            bool expectedResult = true;
+            bool accualresult = TestAdministration.Add(newtestdog);
+            Assert.AreEqual(expectedResult, accualresult);
+        }
+        [TestMethod]
+        public void Test_AdministrationDelTrue()
+        {
+            //act
+            MakeCat();
+            TestAdministration.Add(Testcat);
+            //assert
+            bool expectedResult = true;
+            bool accualResult = TestAdministration.RemoveAnimal(123);
+            Assert.AreEqual(expectedResult, accualResult);
+        }
+        [TestMethod]
+        public void Test_AdministrationDelFalse()
+        {
+            //act
+            MakeCat();
+            TestAdministration.Add(Testcat);
+            //assert
+            bool expectedResult = false;
+            bool accualResult = TestAdministration.RemoveAnimal(587);
+            Assert.AreEqual(expectedResult, accualResult);
+        }
+        [TestMethod]
+        public void Test_AdministrationFindAnimal()
+        {
+            MakeCat();
+            TestAdministration.Add(Testcat);
+            //assert
+            Animal expectedAnimal = Testcat;
+            Animal accualAnimal = TestAdministration.findAnimal(123);
+            Assert.AreEqual(expectedAnimal, accualAnimal);
+        }
+        [TestMethod]
+        public void Test_AdministrationFindAnimalNULL()
+        {
+            MakeCat();
+            TestAdministration.Add(Testcat);
+            //assert
+            Animal expectedAnimal = null;
+            Animal accualAnimal = TestAdministration.findAnimal(456);
+            Assert.AreEqual(expectedAnimal, accualAnimal);
+        }
+        [TestMethod]
+        public void Test_AdministrationAlreadyInSystemTrue()
+        {
+            MakeCat();
+            TestAdministration.Add(Testcat);
+            //assert
+            bool expectedresult = true;
+            bool accualResult = TestAdministration.alreadyInSystem(123);
+            Assert.AreEqual(expectedresult, accualResult);
+        }
+        [TestMethod]
+        public void Test_AdministrationAlreadyInSystemFalse()
+        {
+            MakeCat();
+            TestAdministration.Add(Testcat);
+            //assert
+            bool expectedresult = false;
+            bool accualResult = TestAdministration.alreadyInSystem(456);
+            Assert.AreEqual(expectedresult, accualResult);
         }
     }
 }
